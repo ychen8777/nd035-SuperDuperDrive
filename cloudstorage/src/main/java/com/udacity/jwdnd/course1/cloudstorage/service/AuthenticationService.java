ad25c649dfd8145dd159102cd07c0cbe023c89d7
ychen8777 <60375005+ychen8777@users.noreply.github.com>
2021-05-28 19:56:14 -0400

    add @Service annotation

--- cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/service/AuthenticationService.java
+++ cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/service/AuthenticationService.java
@@ -5,7 +5,9 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Service;
 
+@Service
 public class AuthenticationService implements AuthenticationProvider {
 
     private UserMapper userMapper;
