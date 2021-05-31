0945ae182872b6075898e02783c5ded440c32455
ychen8777 <60375005+ychen8777@users.noreply.github.com>
2021-05-30 20:55:43 -0400

    add UserService as attribute

--- cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/SignupController.java
+++ cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/SignupController.java
@@ -1,13 +1,32 @@
 package com.udacity.jwdnd.course1.cloudstorage.controller;
 
+import com.udacity.jwdnd.course1.cloudstorage.model.User;
+import com.udacity.jwdnd.course1.cloudstorage.service.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class SignupController {
 
+    private final UserService userService;
+
+    public SignupController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping("/signup")
     public String goToSignup(){
+
+        return "signup";
+    }
+
+    @PostMapping("/signup")
+    public String signupUser(@ModelAttribute User user, Model model) {
+        
+
         return "signup";
     }
 
