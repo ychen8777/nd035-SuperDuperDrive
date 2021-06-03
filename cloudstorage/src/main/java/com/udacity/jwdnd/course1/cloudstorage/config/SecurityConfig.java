bb6b7b35862003b36bdb0c140be7659b52ed91e4
ychen8777 <60375005+ychen8777@users.noreply.github.com>
2021-06-03 16:03:56 -0400

    redirect logout to login page

--- cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/config/SecurityConfig.java
+++ cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/config/SecurityConfig.java
@@ -38,6 +38,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll();
 
         http.formLogin().defaultSuccessUrl("/home", true);
+        http.logout().logoutSuccessUrl("/login");
     }
 
 }
