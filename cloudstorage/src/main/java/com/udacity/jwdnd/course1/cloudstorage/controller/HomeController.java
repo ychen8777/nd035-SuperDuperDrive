83cc771c1bce36015fcf55420e220fd6ee3267f8
ychen8777 <60375005+ychen8777@users.noreply.github.com>
2021-06-03 20:42:18 -0400

    add note, @PostMapping(value = "noteForm")
    note added to database, userid not captured
    redirection fall back error

--- cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
+++ cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
@@ -1,22 +1,48 @@
 package com.udacity.jwdnd.course1.cloudstorage.controller;
 
+import com.udacity.jwdnd.course1.cloudstorage.model.Note;
 import com.udacity.jwdnd.course1.cloudstorage.model.User;
+import com.udacity.jwdnd.course1.cloudstorage.service.NoteService;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
 
+    private NoteService noteService;
+    //private User user;
+
+    public HomeController(NoteService noteService) {
+        this.noteService = noteService;
+    }
+
     @GetMapping("/home")
-    public String gotoHome(){
+    public String gotoHome(@ModelAttribute("note") Note note, Model model){
         //Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         //User user = (User) auth.getPrincipal();
         //System.out.println("userid: " + user.getUserid());
 
         return "home";
+    }
+
+    @PostMapping(value = "noteForm")
+    public String addNote(@ModelAttribute("note") Note note, Model model){
+        Note newNote = new Note(null, note.getNoteTitle(), note.getNoteDescription(), note.getUserid());
+        noteService.addNote(newNote);
+
+        return "redirect:/";
+    }
+
+    public Integer getUserid(){
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User user = (User) auth.getPrincipal();
+        return user.getUserid();
     }
 
 
