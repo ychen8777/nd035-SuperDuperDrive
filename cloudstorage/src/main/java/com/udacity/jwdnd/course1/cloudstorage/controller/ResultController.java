732038472a170b9e73e762f8c65afce448599bb9
ychen8777 <60375005+ychen8777@users.noreply.github.com>
2021-06-04 22:35:39 -0400

    Create ResultController.java

---
+++ cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/ResultController.java
@@ -0,0 +1,13 @@
+package com.udacity.jwdnd.course1.cloudstorage.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ResultController {
+
+    @GetMapping("/result")
+    public String gotoResult(){
+        return "result";
+    }
+}
