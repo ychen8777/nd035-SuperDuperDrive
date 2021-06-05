e888e6de5989854411c2e09dac92b4a0c4e503ba
ychen8777 <60375005+ychen8777@users.noreply.github.com>
2021-06-04 22:34:02 -0400

    update value for addNote

--- cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
+++ cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
@@ -8,6 +8,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -36,13 +37,23 @@ public class HomeController {
         return "home";
     }
 
-    @PostMapping(value = "noteForm")
+    // Note
+
+    @PostMapping(value = "addNote")
     public String addNote(@ModelAttribute("note") Note note, Model model){
         Note newNote = new Note(null, note.getNoteTitle(), note.getNoteDescription(), getUserid());
         noteService.addNote(newNote);
 
-        return "redirect:/home";
+        return "redirect:/result";
     }
+    
+
+
+
+
+
+
+
 
     public Integer getUserid(){
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
