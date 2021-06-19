a9ddcc65c7923faae2c0c423d814a097954f1cf7
ychen8777 <60375005+ychen8777@users.noreply.github.com>
2021-06-18 21:20:37 -0400

    define success and non-success GetMapping

--- cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/ResultController.java
+++ cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/ResultController.java
@@ -1,13 +1,32 @@
 package com.udacity.jwdnd.course1.cloudstorage.controller;
 
+import com.udacity.jwdnd.course1.cloudstorage.model.Note;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
 public class ResultController {
 
-    @GetMapping("/result")
-    public String gotoResult(){
+    @GetMapping("/result/success")
+    public String successResult(Model model){
+        String status = "success";
+        model.addAttribute("status", status);
+        return "result";
+    }
+
+    @GetMapping("/result/noSuccess")
+    public String noSuccessResult(Model model){
+        String status = "noSuccess";
+        model.addAttribute("status", status);
+        return "result";
+    }
+
+    @GetMapping("/result/error")
+    public String errorResult(Model model){
+        String status = "error";
+        model.addAttribute("status", status);
         return "result";
     }
 }
