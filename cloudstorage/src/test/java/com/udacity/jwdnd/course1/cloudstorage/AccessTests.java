918b7661a8208a86800d92f96e7bbb8c6c72e224
ychen8777 <60375005+ychen8777@users.noreply.github.com>
2021-06-19 16:10:24 -0400

    define beforeAll and afterAll

--- cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/AccessTests.java
+++ cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/AccessTests.java
@@ -1,8 +1,13 @@
 package com.udacity.jwdnd.course1.cloudstorage;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class AccessTests {
@@ -10,5 +15,23 @@ public class AccessTests {
     @LocalServerPort
     private int port;
 
-    private WebDriver driver;
+    private static WebDriver driver;
+
+    @BeforeAll
+    public static void beforeAll() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        driver.quit();
+    }
+
+    @Test
+    public void getLoginPage() {
+        driver.get("http://localhost:" + port + "/login");
+        Assertions.assertEquals("Login", driver.getTitle());
+    }
+
 }
