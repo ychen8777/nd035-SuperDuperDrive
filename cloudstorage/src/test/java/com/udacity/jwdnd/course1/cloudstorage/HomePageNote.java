033ff7d6590371754c9a34daacb49245bc930a9f
ychen8777 <60375005+ychen8777@users.noreply.github.com>
2021-06-20 18:08:54 -0400

    Create HomePageNote.java

---
+++ cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/HomePageNote.java
@@ -0,0 +1,29 @@
+package com.udacity.jwdnd.course1.cloudstorage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class HomePageNote {
+
+    @FindBy(id = "nav-notes-tab")
+    private WebElement notesTab;
+
+    @FindBy(id = "addNoteButton")
+    private WebElement addNoteButton;
+
+
+    public HomePageNote(WebDriver driver){
+        PageFactory.initElements(driver, this);
+    }
+
+    public void getNoteTab() {
+        notesTab.click();
+    }
+
+    public void getNoteModal() {
+        addNoteButton.click();
+    }
+
+}
