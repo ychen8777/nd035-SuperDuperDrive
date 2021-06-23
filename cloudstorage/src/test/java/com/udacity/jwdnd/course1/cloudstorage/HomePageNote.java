390d39b6bf1645da458dfd4b45518b9468d9f932
ychen8777 <60375005+ychen8777@users.noreply.github.com>
2021-06-22 21:26:59 -0400

    update id for save change button in note modal

--- cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/HomePageNote.java
+++ cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/HomePageNote.java
@@ -23,7 +23,7 @@ public class HomePageNote {
     @FindBy(id = "note-description")
     private WebElement noteDescriptionField;
 
-    @FindBy(id = "noteSubmit")
+    @FindBy(id = "noteSaveButton")
     private WebElement noteSubmitButton;
 
     @FindBy(xpath = "//button[text()='Close']")
@@ -59,5 +59,9 @@ public class HomePageNote {
 
     public WebElement getNoteModal() {
         return noteModal;
+    }
+
+    public WebElement getNoteSubmitButton() {
+        return noteSubmitButton;
     }
 }
