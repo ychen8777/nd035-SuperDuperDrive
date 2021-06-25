f572177058d89658b367f8e7695aef4ee9433224
ychen8777 <60375005+ychen8777@users.noreply.github.com>
2021-06-24 20:07:25 -0400

    Create CredentialMapper.java

---
+++ cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/CredentialMapper.java
@@ -0,0 +1,30 @@
+package com.udacity.jwdnd.course1.cloudstorage.mapper;
+
+import com.udacity.jwdnd.course1.cloudstorage.model.Credential;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+@Mapper
+public interface CredentialMapper {
+
+    @Insert("INSERT INTO CREDENTIALS (credentialid, url, username, key, password, userid) " +
+            "VALUES(#{credentialid}, #{url}, #{username}, #{key}, #{password}, #{userid)")
+    @Options(useGeneratedKeys = true, keyProperty = "credentialid")
+    int insertCredential(Credential credential);
+
+    @Select("SELECT * FROM CREDENTIALS WHERE credentialid = #{credentialid}")
+    Credential getCredential(Integer credentialid);
+
+    @Select("SELECT * FROM CREDENTIALS WHERE userid = #{userid}")
+    List<Credential> getCredentialList(Integer userid);
+
+    @Delete("DELETE FROM CREDENTIALS WHERE credentialid = #{credentialid}")
+    int deleteCredential(Integer credentialid);
+
+    @Update("UPDATE CREDENTIALS SET url = #{url}, username = #{username}, password = {password} " +
+    "WHERE credentialid = #{credentialid}")
+    int updateCredential(Integer credentialid, String url, String username, String password);
+
+
+}
