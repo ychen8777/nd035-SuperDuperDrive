56673329705e7e70c0b83d28a3b025474314c39f
ychen8777 <60375005+ychen8777@users.noreply.github.com>
2021-06-25 19:59:04 -0400

    define addCredential

--- cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/service/CredentialService.java
+++ cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/service/CredentialService.java
@@ -1,7 +1,11 @@
 package com.udacity.jwdnd.course1.cloudstorage.service;
 
 import com.udacity.jwdnd.course1.cloudstorage.mapper.CredentialMapper;
+import com.udacity.jwdnd.course1.cloudstorage.model.Credential;
 import org.springframework.stereotype.Service;
+
+import java.security.SecureRandom;
+import java.util.Base64;
 
 @Service
 public class CredentialService {
@@ -14,5 +18,24 @@ public class CredentialService {
         this.credentialMapper = credentialMapper;
     }
 
-    
+    public int addCredential(String url, String username, String password, Integer userid) {
+        // generate key and encrypt password
+        SecureRandom random = new SecureRandom();
+        byte[] key = new byte[16];
+        random.nextBytes(key);
+        String encodedKey = Base64.getEncoder().encodeToString(key);
+        String encryptedPassword = encryptionService.encryptValue(password, encodedKey);
+
+        // save new credential
+        Credential credential = new Credential(null, url, username, encodedKey, encryptedPassword, userid);
+        //return 0;
+        return credentialMapper.insertCredential(credential);
+    }
+
+    public String decryptPassword(String encryptedPassword, String key) {
+        return encryptionService.decryptValue(encryptedPassword, key);
+    }
+
+
+
 }
