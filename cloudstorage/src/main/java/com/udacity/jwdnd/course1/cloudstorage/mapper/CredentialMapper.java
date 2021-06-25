17000ca47bd85851aa256fe536e38134d5e1f5f9
ychen8777 <60375005+ychen8777@users.noreply.github.com>
2021-06-25 18:11:48 -0400

    update column name key to encodedkey

--- cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/CredentialMapper.java
+++ cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/CredentialMapper.java
@@ -8,7 +8,13 @@ import java.util.List;
 @Mapper
 public interface CredentialMapper {
 
-    @Insert("INSERT INTO CREDENTIALS (credentialid, url, username, key, password, userid) " +
+    /*
+        @Insert("INSERT INTO NOTES (noteid, notetitle, notedescription, userid) " +
+            "VALUES(#{noteid}, #{noteTitle}, #{noteDescription}, #{userid})")
+     */
+
+
+    @Insert("INSERT INTO CREDENTIALS (credentialid, url, username, encodedkey, password, userid) " +
             "VALUES(#{credentialid}, #{url}, #{username}, #{key}, #{password}, #{userid}")
     @Options(useGeneratedKeys = true, keyProperty = "credentialid")
     int insertCredential(Credential credential);
@@ -22,7 +28,7 @@ public interface CredentialMapper {
     @Delete("DELETE FROM CREDENTIALS WHERE credentialid = #{credentialid}")
     int deleteCredential(Integer credentialid);
 
-    @Update("UPDATE CREDENTIALS SET url = #{url}, username = #{username}, password = {password} " +
+    @Update("UPDATE CREDENTIALS SET url = #{url}, username = #{username}, password = #{password} " +
     "WHERE credentialid = #{credentialid}")
     int updateCredential(Integer credentialid, String url, String username, String password);
 
