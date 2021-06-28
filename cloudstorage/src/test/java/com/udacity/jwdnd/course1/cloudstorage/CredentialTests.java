b04062597600db265182b5a5c28f9552d32d0f54
ychen8777 <60375005+ychen8777@users.noreply.github.com>
2021-06-28 18:32:48 -0400

    Create CredentialTests.java

---
+++ cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/CredentialTests.java
@@ -0,0 +1,52 @@
+package com.udacity.jwdnd.course1.cloudstorage;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class CredentialTests {
+
+    @LocalServerPort
+    private int port;
+
+    private static WebDriver driver;
+    private LoginPage loginPage;
+
+    @BeforeAll
+    public static void beforeAll() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        driver.quit();
+    }
+
+    @BeforeEach
+    public void beforeEach() throws InterruptedException {
+        Thread.sleep(1600);
+    }
+
+    // user login
+    @Test
+    @Order(1)
+    public void loginUser() {
+        driver.get("http://localhost:" + port + "/login");
+        this.loginPage = new LoginPage(driver);
+        String testUsername = "SeleniumTest2";
+        String testPassword = "SeleniumTest2";
+        loginPage.loginUser(testUsername, testPassword);
+
+        String actualUrl = driver.getCurrentUrl();
+        String expectedUrl = "http://localhost:" + port + "/home";
+        Assertions.assertEquals(actualUrl, expectedUrl);
+    }
+
+}
