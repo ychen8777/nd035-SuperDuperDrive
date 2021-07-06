71e10d661c1ed68b818e2ea65b35c12d53f713fe
ychen8777 <60375005+ychen8777@users.noreply.github.com>
2021-07-06 15:47:40 -0400

    Create File.java

---
+++ cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/File.java
@@ -0,0 +1,70 @@
+package com.udacity.jwdnd.course1.cloudstorage.model;
+
+
+import java.sql.Blob;
+
+public class File {
+    private Integer fileId;
+    private String filename;
+    private String contenttype;
+    private String filesize;
+    private Integer userid;
+    private Blob filedata;
+
+    public File(Integer fileId, String filename, String contenttype, String filesize, Integer userid, Blob filedata) {
+        this.fileId = fileId;
+        this.filename = filename;
+        this.contenttype = contenttype;
+        this.filesize = filesize;
+        this.userid = userid;
+        this.filedata = filedata;
+    }
+
+    public Integer getFileId() {
+        return fileId;
+    }
+
+    public void setFileId(Integer fileId) {
+        this.fileId = fileId;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+
+    public String getContenttype() {
+        return contenttype;
+    }
+
+    public void setContenttype(String contenttype) {
+        this.contenttype = contenttype;
+    }
+
+    public String getFilesize() {
+        return filesize;
+    }
+
+    public void setFilesize(String filesize) {
+        this.filesize = filesize;
+    }
+
+    public Integer getUserid() {
+        return userid;
+    }
+
+    public void setUserid(Integer userid) {
+        this.userid = userid;
+    }
+
+    public Blob getFiledata() {
+        return filedata;
+    }
+
+    public void setFiledata(Blob filedata) {
+        this.filedata = filedata;
+    }
+}
