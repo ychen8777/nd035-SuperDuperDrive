0e8fd87c27ebdf7bc83978fa677a1fdb5a71d194
ychen8777 <60375005+ychen8777@users.noreply.github.com>
2021-07-06 22:02:11 -0400

    Create FileService.java

---
+++ cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/service/FileService.java
@@ -0,0 +1,22 @@
+package com.udacity.jwdnd.course1.cloudstorage.service;
+
+import com.udacity.jwdnd.course1.cloudstorage.mapper.FileMapper;
+import com.udacity.jwdnd.course1.cloudstorage.model.UserFile;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.sql.Blob;
+
+@Service
+public class FileService {
+
+    private final FileMapper fileMapper;
+
+    public FileService(FileMapper fileMapper) {
+        this.fileMapper = fileMapper;
+    }
+    
+
+}
